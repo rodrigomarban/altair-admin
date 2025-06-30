1ffa818399e5ef9f57c1fe28cbe689812d5926c2
Rodrigo Marban <rodrigo.marban@netclick.mx>
2025-06-30 01:11:56 -0600

    Add pg connection and default to pg in database config

--- config/database.ts
+++ config/database.ts
@@ -1,9 +1,4 @@
-/**
- * Config source: https://git.io/JesV9
- *
- * Feel free to let us know via PR, if you find something broken in this config
- * file.
- */
+// start/config/database.ts
 
 import Env from '@ioc:Adonis/Core/Env'
 import type { DatabaseConfig } from '@ioc:Adonis/Lucid/Database'
@@ -11,35 +6,53 @@ import type { DatabaseConfig } from '@ioc:Adonis/Lucid/Database'
 const databaseConfig: DatabaseConfig = {
   /*
   |--------------------------------------------------------------------------
-  | Connection
+  | Primary connection name
   |--------------------------------------------------------------------------
-  |
-  | The primary connection for making database queries across the application
-  | You can use any key from the `connections` object defined in this same
-  | file.
-  |
   */
-  connection: Env.get('DB_CONNECTION'),
+  connection: Env.get('DB_CONNECTION', 'pg'),  // Pon 'pg' como fallback
 
   connections: {
-
-    mysql: {
-      client: 'mysql',
+    /*
+    |--------------------------------------------------------------------------
+    | PostgreSQL
+    |--------------------------------------------------------------------------
+    */
+    pg: {
+      client: 'pg',
       connection: {
-        host: Env.get('MYSQL_HOST'),
-        port: Env.get('MYSQL_PORT'),
-        user: Env.get('MYSQL_USER'),
-        password: Env.get('MYSQL_PASSWORD', ''),
-        database: Env.get('MYSQL_DB_NAME'),
+        host: Env.get('PG_HOST'),
+        port: Env.get('PG_PORT'),
+        user: Env.get('PG_USER'),
+        password: Env.get('PG_PASSWORD', ''),
+        database: Env.get('PG_DB_NAME'),
       },
       migrations: {
         naturalSort: true,
       },
       healthCheck: false,
       debug: false,
-    }
+    },
 
-  }
+    /*
+    |--------------------------------------------------------------------------
+    | MySQL (opcional)
+    |--------------------------------------------------------------------------
+    | Puedes comentar todo este bloque si no lo vas a usar.
+    */
+    // mysql: {
+    //   client: 'mysql',
+    //   connection: {
+    //     host: Env.get('MYSQL_HOST'),
+    //     port: Env.get('MYSQL_PORT'),
+    //     user: Env.get('MYSQL_USER'),
+    //     password: Env.get('MYSQL_PASSWORD', ''),
+    //     database: Env.get('MYSQL_DB_NAME'),
+    //   },
+    //   migrations: { naturalSort: true },
+    //   healthCheck: false,
+    //   debug: false,
+    // },
+  },
 }
 
 export default databaseConfig
